6311c083295790d9a616567b2ab77a1674d5237f
TheBeachmaster <otienoken@outlook.com>
2018-01-11 20:48:51 +0300

    Add SMS Service

---
+++ AfricasTalkingGateway/AfricasTalkingGateway/SMSService.vb
@@ -0,0 +1,6 @@
+﻿Public Class SMSService
+    Public Function SendMessage(ByVal to_ As String, ByVal from_ As String, ByVal message As String, Optional ByVal bulkSMSMode_ As _
+                                   Integer = Nothing, Optional ByVal options_ As Dictionary(Of String, String) = Nothing) As Object
+
+    End Function
+End Class
