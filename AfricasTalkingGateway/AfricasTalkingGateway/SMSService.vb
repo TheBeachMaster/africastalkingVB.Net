fde2304b525a4c83be7628eeca37d21f919fc2bb
TheBeachmaster <otienoken@outlook.com>
2018-01-12 19:52:24 +0300

    Remove SMS Service class

--- AfricasTalkingGateway/AfricasTalkingGateway/SMSService.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class SMSService
-    Public Function SendMessage(ByVal to_ As String, ByVal from_ As String, ByVal message As String, Optional ByVal bulkSMSMode_ As _
-                                   Integer = Nothing, Optional ByVal options_ As Dictionary(Of String, String) = Nothing) As Object
-
-    End Function
-End Class
