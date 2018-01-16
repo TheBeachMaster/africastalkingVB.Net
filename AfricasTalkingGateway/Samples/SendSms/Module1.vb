b515b928b8f1cdee8ae6fca1ba802ae39be944b2
TheBeachmaster <otienoken@outlook.com>
2018-01-16 22:47:55 +0300

    Add sample project for SMS : WIP

---
+++ AfricasTalkingGateway/Samples/SendSms/Module1.vb
@@ -0,0 +1,8 @@
+﻿Imports AfricasTalkingGateway
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        Dim gateway As New AfricasTalkingGateway.AfricasTalkingGateway("")
+    End Sub
+End Module
