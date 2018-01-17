98cd1c0c332a57348d33426e01e0742afb8dd711
TheBeachmaster <otienoken@outlook.com>
2018-01-17 10:24:11 +0300

    Clean-up project :  VS 2017 unable to generate project dll

--- AfricasTalkingGateway/Samples/SendSms/Module1.vb
+++
@@ -1,8 +0,0 @@
-﻿Imports AfricasTalkingGateway
-
-Module Program
-    Sub Main(args As String())
-        Console.WriteLine("Hello World!")
-        Dim gateway As New AfricasTalkingGateway.AfricasTalkingGateway("")
-    End Sub
-End Module
