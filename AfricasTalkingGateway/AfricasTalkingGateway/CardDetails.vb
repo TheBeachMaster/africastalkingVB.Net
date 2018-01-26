d50415e5bf7925abbb964f32f68325ef4726bd18
TheBeachmaster <otienoken@outlook.com>
2018-01-26 14:59:19 +0300

    Define client card properties

---
+++ AfricasTalkingGateway/AfricasTalkingGateway/CardDetails.vb
@@ -0,0 +1,27 @@
+﻿Imports Newtonsoft.Json
+
+Partial Public Class CardDetails
+    <JsonProperty("amount")>
+    Public Property Amount() As Decimal
+
+    <JsonProperty("checkoutToken")>
+    Public Property CheckoutToken() As String
+
+    <JsonProperty("currencyCode")>
+    Public Property CurrencyCode() As String
+
+    <JsonProperty("metadata")>
+    Public Property Metadata() As Dictionary(Of String, String)
+
+    <JsonProperty("narration")>
+    Public Property Narration() As String
+
+    <JsonProperty("paymentCard")>
+    Public Property PaymentCard() As PaymentCard
+
+    <JsonProperty("productName")>
+    Public Property ProductName() As String
+
+    <JsonProperty("username")>
+    Public Property Username() As String
+End Class
