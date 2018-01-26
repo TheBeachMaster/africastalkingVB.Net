275e02ab1a23ee143556778a7ba1af6e6196928b
TheBeachmaster <otienoken@outlook.com>
2018-01-26 15:01:30 +0300

    Define genral properties for bank transfer

---
+++ AfricasTalkingGateway/AfricasTalkingGateway/BankTransfer.vb
@@ -0,0 +1,12 @@
+﻿Imports Newtonsoft.Json
+
+Partial Public Class BankTransfer
+    <JsonProperty("productName")>
+    Public Property ProductName() As String
+
+    <JsonProperty("recipients")>
+    Public Property Recipients() As List(Of BankTransferRecipients)
+
+    <JsonProperty("username")>
+    Public Property Username() As String
+End Class
