85fb13fa0bb705900b69f13963482ace3c31cb74
TheBeachmaster <otienoken@outlook.com>
2018-01-26 14:58:45 +0300

    Define OTP validation properties

---
+++ AfricasTalkingGateway/AfricasTalkingGateway/OTP.vb
@@ -0,0 +1,13 @@
+﻿Imports Newtonsoft.Json
+
+Partial Public Class OTP
+    <JsonProperty("otp")>
+    Public Property Otp() As String
+
+    <JsonProperty("transactionId")>
+    Public Property TransactionId() As String
+
+    <JsonProperty("username")>
+    Public Property Username() As String
+End Class
+
