cd64781a04855aecd86caa8bdea3bd8fc423f829
TheBeachmaster <otienoken@outlook.com>
2018-01-26 15:01:55 +0300

    Define properties for bank checkout

---
+++ AfricasTalkingGateway/AfricasTalkingGateway/BankCheckout.vb
@@ -0,0 +1,17 @@
+﻿Imports Newtonsoft.Json
+Partial Public Class BankCheckout
+    <JsonProperty("amount")>
+    Public Property Amount() As Decimal
+    <JsonProperty("bankAccount")>
+    Public Property BankAccount() As BankAccount
+    <JsonProperty("currencyCode")>
+    Public Property CurrencyCode() As String
+    <JsonProperty("metadata")>
+    Public Property Metadata() As Dictionary(Of String, String)
+    <JsonProperty("narration")>
+    Public Property Narration() As String
+    <JsonProperty("productName")>
+    Public Property ProductName() As String
+    <JsonProperty("username")>
+    Public Property Username() As String
+End Class
