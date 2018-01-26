79cda8b134be91cc3b10aa4ccca06378373beea4
TheBeachmaster <otienoken@outlook.com>
2018-01-26 15:00:10 +0300

    Define bank recipient properties

---
+++ AfricasTalkingGateway/AfricasTalkingGateway/BankTransferRecipients.vb
@@ -0,0 +1,32 @@
+﻿
+Imports Newtonsoft.Json
+''' This contains a list of Recipient elements.
+''' 
+Partial Public Class BankTransferRecipients
+    Public Sub New(amount As Decimal, bankAccount As BankAccount, currencyCode As String, narration As String)
+        Me.Amount = amount
+        Me.BankAccount = bankAccount
+        Me.CurrencyCode = currencyCode
+        Metadata = New Dictionary(Of String, String)()
+        Me.Narration = narration
+    End Sub
+
+    <JsonProperty("amount")>
+    Public Property Amount() As Decimal
+
+    <JsonProperty("bankAccount")>
+    Public Property BankAccount() As BankAccount
+
+    <JsonProperty("currencyCode")>
+    Public Property CurrencyCode() As String
+
+    <JsonProperty("metadata")>
+    Public Property Metadata() As Dictionary(Of String, String)
+
+    <JsonProperty("narration")>
+    Public Property Narration() As String
+
+    Public Sub AddMetadata(key As String, value As String)
+        Metadata.Add(key, value)
+    End Sub
+End Class
