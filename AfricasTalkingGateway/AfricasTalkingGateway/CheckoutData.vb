2fd4ac8dcc29994e8e4de2ee2082602da6915e8c
TheBeachmaster <otienoken@outlook.com>
2018-02-04 19:19:05 +0300

    [fix] Serialize Payload before push to API

--- AfricasTalkingGateway/AfricasTalkingGateway/CheckoutData.vb
+++ AfricasTalkingGateway/AfricasTalkingGateway/CheckoutData.vb
@@ -1,10 +1,23 @@
 ﻿
+Imports Newtonsoft.Json
+
 Public Class CheckoutData
+    <JsonProperty("username")>
     Public Property Username() As String
+    <JsonProperty("productName")>
     Public Property ProductName() As String
+    <JsonProperty("phoneNumber")>
     Public Property PhoneNumber() As String
+    <JsonProperty("currencyCode")>
     Public Property CurrencyCode() As String
+    <JsonProperty("amount")>
     Public Property Amount() As Decimal
+    <JsonProperty("providerChannel")>
     Public Property ProviderChannel() As String
+    <JsonProperty("metadata")>
     Public Property Metadata() As Dictionary(Of String, String)
+    Public Function ToJson() As String
+        Dim checkoutObject As String = JsonConvert.SerializeObject(Me)
+        Return checkoutObject
+    End Function
 End Class
